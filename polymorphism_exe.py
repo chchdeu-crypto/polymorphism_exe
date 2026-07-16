1aeadd2be3c9ebb7882c38017a8d8d76c21f8c2d
chaim <chchdeu@gmail.com>
2026-07-16 10:14:06 +0300

    mission_2

--- polymorphism_exe.py
+++ polymorphism_exe.py
@@ -18,3 +18,24 @@ d1=SmartTV("samsung")
 d2=SmartSpeaker("echo")
 d1.activate()
 d2.activate()
+
+#mission 2
+class divice:
+    def __init__(self,name):
+        self.name=name
+    def deactivate(self):
+        print(f"Device {self.name} is now off.")
+class SmartLamp(divice):
+    def __init__(self, name):
+        super().__init__(name)
+    def deactivate(self):
+        print(f"Lamp {self.name} is dimming and turning off.")
+class SmartAC(divice):
+    def __init__(self, name):
+        super().__init__(name)
+    def deactivate(self):
+        print(f"AC {self.name} is cooling down and switching off.")
+lamp=SmartLamp("Bedroom Lamp")
+ac=SmartAC("living room AC")
+lamp.deactivate()
+ac.deactivate()
